d33815a83a61f8239fa05ce8a5b686d50d8c8b02
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-06-19 00:39:16 +0200

    added form for logging in

--- app/presenters/UserPresenter.php
+++ app/presenters/UserPresenter.php
@@ -1,12 +1,34 @@
 <?php
 namespace Nexendrie\Presenters;
 
+use Nette\Application\UI;
+
 /**
  * Presenter User
  *
  * @author Jakub Konečný
  */
 class UserPresenter extends \Nette\Application\UI\Presenter {
+  /**
+   * Creates form for logging in
+   * 
+   * @return \Nette\Application\UI\Form
+   */
+  protected function createComponentLoginForm() {
+    $form = new UI\Form;
+    $form->addText("username", "Uživatelské jméno:")
+      ->setRequired("Zadej jméno");
+    $form->addPassword("password", "Heslo:")
+      ->setRequired("Zadej heslo");
+    $form->addSubmit("login", "Přihlásit se");
+    $form->onSuccess[] = array($this, "loginFormSucceeded");
+    return $form;
+  }
+  
+  function loginFormSucceeded(UI\Form $form, $values) {
+    
+  }
+  
   /**
    * @todo return to previous page if possible
    * @return void
