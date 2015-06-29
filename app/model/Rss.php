7f04a9938fbd44f0e035c9ed7218f4a49c76e805
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-06-29 19:45:21 +0200

    fixed Rss model

--- app/model/Rss.php
+++ app/model/Rss.php
@@ -33,15 +33,15 @@ class Rss extends \Nette\Object {
   function newsFeed() {
     $paginator = new \Nette\Utils\Paginator;
     $items = $this->newsModel->page($paginator);
-    $channel = simplexml_load_file(APP_DIR . "/templates/newsFeed.xml");
+    $channel = simplexml_load_file(APP_DIR . "/FrontModule/templates/newsFeed.xml");
     unset($channel->channel->link);
     unset($channel->channel->lastBuildDate);
-    $channel->channel->addChild("link", $this->linkGenerator->link("Homepage:default"));
+    $channel->channel->addChild("link", $this->linkGenerator->link("Front:Homepage:default"));
     $channel->channel->addChild("lastBuildDate", $this->localeModel->formatDateTime(time()));
     foreach($items as $item) {
       $i = $channel->channel->addChild("item");
       $i->addChild("title", $item->title);
-      $link = $this->linkGenerator->link("News:view", array("id" => $item->id));
+      $link = $this->linkGenerator->link("Front:News:view", array("id" => $item->id));
       $i->addChild("link", $link);
       $i->addChild("pubDate", $item->added);
       $i->addChild("description", $item->text);
@@ -60,18 +60,18 @@ class Rss extends \Nette\Object {
     $news = $this->newsModel->view($newsId);
     if(!$news) throw new \Nette\Application\BadRequestException("Specified news does not exist.");
     $comments = $this->newsModel->viewComments($newsId);
-    $channel = simplexml_load_file(APP_DIR . "/templates/commentsFeed.xml");
+    $channel = simplexml_load_file(APP_DIR . "/FrontModule/templates/commentsFeed.xml");
     $old_title = (string) $channel->channel->title;
     unset($channel->channel->link);
     unset($channel->channel->lastBuildDate);
     unset($channel->channel->title);
     $channel->channel->addChild("title", $old_title . $news->title);
-    $channel->channel->addChild("link", $this->linkGenerator->link("News:view", array("id" => $newsId)));
+    $channel->channel->addChild("link", $this->linkGenerator->link("Front:News:view", array("id" => $newsId)));
     $channel->channel->addChild("lastBuildDate", $this->localeModel->formatDateTime(time()));
     foreach($comments as $comment) {
       $c = $channel->channel->addChild("item");
       $c->addChild("title", $comment->title);
-      $link = $this->linkGenerator->link("News:view", array("id" => $newsId));
+      $link = $this->linkGenerator->link("Front:News:view", array("id" => $newsId));
       $link .= "#comment-$comment->id";
       $c->addChild("link", $link);
       $c->addChild("pubDate", $comment->added);
