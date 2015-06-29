a49ae823ba885a0f9960373b6f7ddbe1f769c354
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-06-29 23:24:09 +0200

    merged model Permissions with Authorizator

--- app/model/Authorizator.php
+++ app/model/Authorizator.php
@@ -8,16 +8,62 @@ namespace Nexendrie;
  */
 class Authorizator extends \Nette\Object {
   /**
+   * Get list of all groups ordered by level
+   * 
+   * @param \Nette\Caching\Cache $cache
+   * @param \Nette\Database\Context $db
+   * @return array
+   */
+  static function getGroups(\Nette\Caching\Cache $cache, \Nette\Database\Context $db) {
+    $groups = $cache->load("groups_by_level");
+    if($groups === NULL) {
+      $groupsRows = $db->table("groups")
+        ->order("level");
+      foreach($groupsRows as $row) {
+        $group = (object) array(
+          "id" => $row->id, "single_name" => $row->single_name, "level" => $row->level
+        );
+        $groups[$group->id] = $group;
+      }
+      $cache->save("groups_by_level", $groups);
+    }
+    return $groups;
+  }
+  
+  /**
+   * Get permissions
+   * 
+   * @param \Nette\Caching\Cache $cache
+   * @param \Nette\Database\Context $db
+   * @return array
+   */
+  static function getPermissions(\Nette\Caching\Cache $cache, \Nette\Database\Context $db) {
+    $return = $cache->load("permissions");
+    if($return === NULL) {
+      $rows = $db->table("permissions");
+      foreach($rows as $row) {
+        $per = new \stdClass;
+        foreach($row as $key => $value) {
+          $per->$key = $value;
+        }
+        $return[] = $per;
+      }
+      $cache->save("permissions", $return);
+    }
+    return $return;
+  }
+  
+  /**
   * Factory for Authorizator
   * 
   * @param \HeroesofAbenez\Permissions $model
   * @return \Nette\Security\Permission
   */
-  static function create(\Nexendrie\Permissions $model) {
+  static function create(\Nette\Caching\Cache $cache, \Nette\Database\Context $db) {
     $permission = new \Nette\Security\Permission;
     
-    $groups = $model->getGroups();
-    $permissions = $model->getPermissions();
+    $groups = self::getGroups($cache, $db);
+    $permissions = self::getPermissions($cache, $db);
     
     foreach($groups as $i => $row) {
       if($row->level === 0) {
