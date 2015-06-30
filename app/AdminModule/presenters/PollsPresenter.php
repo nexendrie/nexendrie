4895f3a127bf419e39f4b8c2dac65b2063bf8a38
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-06-30 15:39:45 +0200

    added page admin/polls

---
+++ app/AdminModule/presenters/PollsPresenter.php
@@ -0,0 +1,40 @@
+<?php
+namespace Nexendrie\AdminModule\Presenters;
+
+/**
+ * Presenter Polls
+ *
+ * @author Jakub Konečný
+ */
+class PollsPresenter extends BasePresenter {
+  /** @var \Nexendrie\Polls */
+  protected $model;
+  
+  /**
+   * @param \Nexendrie\Polls $model
+   */
+  function __construct(\Nexendrie\Polls $model) {
+    $this->model = $model;
+  }
+  
+  function startup() {
+    parent::startup();
+    $this->requiresPermissions("poll", "add");
+  }
+  
+  /**
+   * @return void
+   */
+  function renderDefault() {
+    $this->template->polls = $this->model->all();
+  }
+  
+  /**
+   * @param int $id
+   * @return void
+   */
+  function actionEdit($id) {
+    
+  }
+}
+?>
---
+++ app/model/Polls.php
@@ -0,0 +1,56 @@
+<?php
+namespace Nexendrie;
+
+/**
+ * Polls Model
+ *
+ * @author Jakub Konečný
+ */
+class Polls extends \Nette\Object {
+  /** @var \Nette\Database\Context */
+  protected $db;
+  /** @var \Nexendrie\Profile */
+  protected $profileModel;
+  /** @var \Nexendrie\Locale */
+  protected $localeModel;
+  
+  /**
+   * @param \Nette\Database\Context $db
+   * @param \Nexendrie\Profile $profileModel
+   * @param \Nexendrie\Locale $localeModel
+   */
+  function __construct(\Nette\Database\Context $db, \Nexendrie\Profile $profileModel, \Nexendrie\Locale $localeModel) {
+    $this->db = $db;
+    $this->profileModel = $profileModel;
+    $this->localeModel = $localeModel;
+  }
+  
+  /**
+   * @return array
+   */
+  function all() {
+    $return = array();
+    $polls = $this->db->table("polls")->order("added DESC");
+    foreach($polls as $poll) {
+      $p = new \stdClass;
+      foreach($poll as $key => $value) {
+        if($key === "text") {
+          $p->$key = substr($value, 0 , 150);
+          continue;
+        } elseif($key === "author") {
+          $user = $this->profileModel->getNames($value);
+          $p->$key = $user->publicname;
+          $key .= "_username";
+          $p->$key = $user->username;
+        } elseif($key === "added") {
+          $p->$key = $this->localeModel->formatDateTime($value);
+        } else {
+          $p->$key = $value;
+        }
+      }
+      $return[] = $p;
+    }
+    return $return;
+  }
+}
+?>
