1e77d25caf71047e5497a730acba681888853a4f
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-08-18 16:03:47 +0200

    added factory for router

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
-use Nette\Application\Routers\RouteList,
-    Nette\Application\Routers\Route;
 
 require WWW_DIR . "/vendor/autoload.php";
 Tracy\Debugger::enable(null, APP_DIR . "/log");
@@ -13,20 +11,6 @@ $configurator->createRobotLoader()
     ->addDirectory(APP_DIR)
     ->register();
 $container = $configurator->createContainer();
-
-$router = new RouteList;
-$router[] = new Route("profile/<username>", "Front:Profile:default");
-$router[] = new Route("message/<id [0-9]+>", "Front:Messages:view");
-$router[] = new Route("poll/<id [0-9]+>", "Front:Poll:view");
-$router[] = new Route("news/page/<page [0-9]+>", "Front:News:page");
-$router[] = new Route("rss[/<action>][/<news [0-9]+>]", "Front:Rss:news");
-$router[] = new Route("admin/groups", "Admin:Group:default");
-$router[] = new Route("admin/users", "Admin:User:default");
-$router[] = new Route("admin/<presenter>[/<action>][/<id>]", array(
-"module" => "Admin", "presenter" => "Homepage", "action" => "default"));
-$router[] = new Route("<presenter>[/<action>][/<id>]", array(
-"module" => "Front", "presenter" => "Homepage", "action" => "default"));
-$container->addService("router", $router);
 
 return $container;
 ?>
---
+++ app/model/RouterFactory.php
@@ -0,0 +1,34 @@
+<?php
+namespace Nexendrie\Model;
+
+use Nette\Application\Routers\RouteList,
+    Nette\Application\Routers\Route;
+
+/**
+ * Router Factory
+ *
+ * @author Jakub Konečný
+ */
+class RouterFactory extends \Nette\Object {
+  /**
+   * @return \Nette\Application\Routers\RouteList
+   */
+  static function create() {
+    $router = new RouteList;
+    $router[] = new Route("profile/<username>", "Front:Profile:default");
+    $router[] = new Route("message/<id [0-9]+>", "Front:Messages:view");
+    $router[] = new Route("poll/<id [0-9]+>", "Front:Poll:view");
+    $router[] = new Route("news/page/<page [0-9]+>", "Front:News:page");
+    $router[] = new Route("rss[/<action>][/<news [0-9]+>]", "Front:Rss:news");
+    $router[] = new Route("admin/groups", "Admin:Group:default");
+    $router[] = new Route("admin/users", "Admin:User:default");
+    $router[] = new Route("admin/<presenter>[/<action>][/<id>]", array(
+      "module" => "Admin", "presenter" => "Homepage", "action" => "default")
+    );
+    $router[] = new Route("<presenter>[/<action>][/<id>]", array(
+      "module" => "Front", "presenter" => "Homepage", "action" => "default")
+    );
+    return $router;
+  }
+}
+?>
