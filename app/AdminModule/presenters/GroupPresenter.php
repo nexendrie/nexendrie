7cc830022f80d55783a6e17d6b2227d5af1425b7
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-09-03 18:39:41 +0200

    fixed GroupPresenter::createComponentEditGroupForm()

--- app/AdminModule/presenters/GroupPresenter.php
+++ app/AdminModule/presenters/GroupPresenter.php
@@ -36,22 +36,21 @@ class GroupPresenter extends BasePresenter {
    * @return \Nette\Application\UI\Form
    */
   function createComponentEditGroupForm() {
-    $group = $this->model->get($this->getParameter("id"));
+    $group = $this->model->ormGet($this->getParameter("id"));
     $form = new Form;
     $form->addText("name", "Jméno:")
       ->addRule(Form::MAX_LENGTH, "Jméno skupiny může mít maximálně 30 znaků.", 30)
       ->setRequired("Zadej jméno skupiny.");
     $form->addText("singleName", "Titul člena:")
       ->addRule(Form::MAX_LENGTH, "Titul člena může mít maximálně 30 znaků.", 30)
-      ->setRequired("Zadej titul člena.")
-      ->setDefaultValue($group->single_name);
+      ->setRequired("Zadej titul člena.");
     $form->addText("level", "Úroveň skpuiny:")
       ->addRule(Form::INTEGER, "Úroveň skupiny musí být číslo")
       ->addRule(Form::MAX_LENGTH, "Úroveň skupiny může mít maximálně 5 znaků.", 5)
       ->setRequired("Zadej úroveň skupiny.");
     $form->addSubmit("send", "Odeslat");
     $form->onSuccess[] = array($this, "editGroupFormSucceeded");
-    $form->setDefaults((array) $group);
+    $form->setDefaults($group->toArray());
     return $form;
   }
   
