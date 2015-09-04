37157bebcd1b2794637ac0ffb26f42d8c6f8525f
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-09-04 15:06:56 +0200

    fixed namespace of Form in AddCommentFormFactory

--- app/forms/AddCommentFormFactory.php
+++ app/forms/AddCommentFormFactory.php
@@ -15,7 +15,7 @@ class AddCommentFormFactory {
   function create() {
     $form = new Form;
     $form->addText("title", "Titulek:")
-      ->addRule(UI\Form::MAX_LENGTH, "Titulek může mít maximálně 30 znaků.", 30)
+      ->addRule(Form::MAX_LENGTH, "Titulek může mít maximálně 30 znaků.", 30)
       ->setRequired("Zadej titulek.");
     $form->addTextArea("text", "Text:")
       ->setRequired("Zadej text.");
