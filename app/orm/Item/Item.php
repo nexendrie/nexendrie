d74802fe18293a8d817e09721fb72ba338dbcab3
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-10-08 15:01:25 +0200

    print also currency for prices in shop

--- app/orm/Item/Item.php
+++ app/orm/Item/Item.php
@@ -11,10 +11,20 @@ use Nextras\Orm\Entity\Entity,
  * @property string $name
  * @property string $description
  * @property int $price
+ * @property-read string $priceT
  * @property Shop $shop {m:1 Shop}
  * @property OneHasMany|UserItem[] $userItems {1:m UserItem::$item}
  */
 class Item extends Entity {
-
+  /** @var \Nexendrie\Model\Locale $localeModel */
+  protected $localeModel;
+  
+  function injectLocaleModel(\Nexendrie\Model\Locale $localeModel) {
+    $this->localeModel = $localeModel;
+  }
+  
+  function getterPriceT() {
+    return $this->localeModel->money($this->price);
+  }
 }
 ?>
