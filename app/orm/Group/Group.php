c654df9c9b5bdbcd7937ab1bb2f9f2e3459d174c
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-10-13 19:06:27 +0200

    added property Group::$path

--- app/orm/Group/Group.php
+++ app/orm/Group/Group.php
@@ -12,10 +12,15 @@ use Nextras\Orm\Entity\Entity,
  * @property string $name
  * @property string $singleName
  * @property int $level
+ * @property int $path {enum self::PATH_*} 
  * @property OneHasMany|User[] $members {1:m User}
  * @property OneHasMany|Permission[] $permissions {1:m Permission}
  */
 class Group extends Entity {
+  const PATH_CITY = 1;
+  const PATH_CHURCH = 2;
+  const PATH_TOWER = 3;
+  
   /**
    * @return \Nexendrie\Orm\GroupDummy
    */
