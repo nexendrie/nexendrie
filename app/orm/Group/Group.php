3aa71fc62b316b6e0f68f791de8e501a0656917a
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-10-15 13:09:51 +0200

    changed Group::$path to string

--- app/orm/Group/Group.php
+++ app/orm/Group/Group.php
@@ -12,14 +12,14 @@ use Nextras\Orm\Entity\Entity,
  * @property string $name
  * @property string $singleName
  * @property int $level
- * @property int $path {enum self::PATH_*} 
+ * @property string $path {enum self::PATH_*} 
  * @property OneHasMany|User[] $members {1:m User}
  * @property OneHasMany|Permission[] $permissions {1:m Permission}
  */
 class Group extends Entity {
-  const PATH_CITY = 1;
-  const PATH_CHURCH = 2;
-  const PATH_TOWER = 3;
+  const PATH_CITY = "city";
+  const PATH_CHURCH = "church";
+  const PATH_TOWER = "tower";
   
   /**
    * @return \Nexendrie\Orm\GroupDummy
