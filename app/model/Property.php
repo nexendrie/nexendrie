d5132f321c10edb61e6f2408a1500485aaddf986
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-10-30 14:27:53 +0100

    showed payed and received taxes from current month at page property/budget

--- app/model/Property.php
+++ app/model/Property.php
@@ -11,14 +11,17 @@ class Property extends \Nette\Object {
   protected $jobModel;
   /** @var \Nexendrie\Model\Bank */
   protected $bankModel;
+  /** @var \Nexendrie\Model\Taxes */
+  protected $taxesModel;
   /** @var \Nexendrie\Orm\Model */
   protected $orm;
   /** @var \Nette\Security\User */
   protected $user;
   
-  function __construct(\Nexendrie\Model\Job $jobModel, \Nexendrie\Model\Bank $bankModel, \Nexendrie\Orm\Model $orm, \Nette\Security\User $user) {
+  function __construct(\Nexendrie\Model\Job $jobModel, \Nexendrie\Model\Bank $bankModel, \Nexendrie\Model\Taxes $taxesModel, \Nexendrie\Orm\Model $orm, \Nette\Security\User $user) {
     $this->jobModel = $jobModel;
     $this->bankModel = $bankModel;
+    $this->taxesModel = $taxesModel;
     $this->orm = $orm;
     $this->user = $user;
   }
@@ -58,9 +61,16 @@ class Property extends \Nette\Object {
         "incomeTax" => 0,
         "loansInterest" => 0
     ));
+    $budget["expenses"]["incomeTax"] = $this->taxesModel->calculateTax(array_sum($budget["incomes"]));
     $loans = $this->orm->loans->findReturnedThisMonth($this->user->id);
     foreach($loans as $loan) {
       $budget["expenses"]["loansInterest"] += $this->bankModel->calculateInterest($loan);
+    }
+    $towns = $this->orm->towns->findByOwner($this->user->id);
+    foreach($towns as $town) {
+      $budget["incomes"]["taxes"] += $this->taxesModel->calculateTownTaxes($town)->taxes;
+      $current = ($town->id === $this->user->identity->town) AND ($town->owner->id === $this->user->id);
+      if($current) $budget["incomes"]["taxes"] += $budget["expenses"]["incomeTax"];
     }
     return $budget;
   }
