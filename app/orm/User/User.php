453718aebfeb476afb4045884e704a1e61c0f4dd
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-11-03 22:31:37 +0100

    measure users' life

--- app/orm/User/User.php
+++ app/orm/User/User.php
@@ -20,6 +20,7 @@ use Nextras\Orm\Entity\Entity,
  * @property bool $infomails {default 0}
  * @property string $style {default "blu-sky"}
  * @property bool $banned {default 0}
+ * @property int $life {default 60}
  * @property int $money {default 2}
  * @property Town $town {m:1 Town::$denizens}
  * @property-read string $moneyT {virtual}
@@ -52,6 +53,12 @@ class User extends Entity {
   
   protected function getterMoneyT() {
     return $this->localeModel->money($this->money);
+  }
+  
+  protected function setterLife($value) {
+    if($value > 60) return 60;
+    elseif($value < 1) return 1;
+    else return $value;
   }
 }
 ?>
