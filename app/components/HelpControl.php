cc28397becf1149edb316c313f2106c8ac15da4f
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-11-08 17:26:12 +0100

    added generalized BookControl

--- app/components/HelpControl.php
+++ app/components/HelpControl.php
@@ -6,31 +6,31 @@ namespace Nexendrie\Components;
  *
  * @author Jakub Konečný
  */
-class HelpControl extends \Nette\Application\UI\Control {
+class HelpControl extends Book\BookControl {
   /** @var \Nexendrie\Orm\Model */
   protected $orm;
   
   function __construct(\Nexendrie\Orm\Model $orm) {
     $this->orm = $orm;
+    parent::__construct(":Front:Help", "help");
   }
   
   /**
-   * 
-   * @return \Nexendrie\Components\Help\HelpPagesStorage
+   * @return \Nexendrie\Components\Book\BookPagesStorage
    */
   function getPages() {
-    $storage = new Help\HelpPagesStorage;
-    $storage[] = new Help\HelpPage("introduction", "Úvod");
-    $storage[] = new Help\HelpPage("titles", "Tituly");
-    $storage[] = new Help\HelpPage("towns", "Města");
-    $storage[] = new Help\HelpPage("monastery", "Klášter");
-    $storage[] = new Help\HelpPage("money", "Peníze");
-    $storage[] = new Help\HelpPage("work", "Práce");
-    $storage[] = new Help\HelpPage("adventures", "Dobrodružství");
-    $storage[] = new Help\HelpPage("bank", "Banka");
-    $storage[] = new Help\HelpPage("academy", "Akademie");
-    $storage[] = new Help\HelpPage("market", "Tržiště");
-    $storage[] = new Help\HelpPage("stables", "Stáje");
+    $storage = new Book\BookPagesStorage;
+    $storage[] = new Book\BookPage("introduction", "Úvod");
+    $storage[] = new Book\BookPage("titles", "Tituly");
+    $storage[] = new Book\BookPage("towns", "Města");
+    $storage[] = new Book\BookPage("monastery", "Klášter");
+    $storage[] = new Book\BookPage("money", "Peníze");
+    $storage[] = new Book\BookPage("work", "Práce");
+    $storage[] = new Book\BookPage("adventures", "Dobrodružství");
+    $storage[] = new Book\BookPage("bank", "Banka");
+    $storage[] = new Book\BookPage("academy", "Akademie");
+    $storage[] = new Book\BookPage("market", "Tržiště");
+    $storage[] = new Book\BookPage("stables", "Stáje");
     return $storage;
   }
   
@@ -57,99 +57,10 @@ class HelpControl extends \Nette\Application\UI\Control {
   function renderAcademy() {
     $this->template->skills = $this->orm->skills->findAll();
   }
-  
-  /**
-   * @param string $page
-   * @return void
-   */
-  function render($page) {
-    $template = $this->template;
-    $pages = $this->getPages();
-    if(!$pages->hasPage($page)) $page = "index";
-    if($page === "index") {
-      $template->setFile(__DIR__ . "/helpIndex.latte");
-    } else {
-      $template->setFile(__DIR__ . "/helpPage.latte");
-      $template->index = $pages->getIndex($page);
-      $template->current = $pages[$template->index];
-    }
-    $template->pages = $pages;
-    $method = "render" . ucfirst($page);
-    if(method_exists($this, $method)) call_user_func(array($this, $method));
-    $template->render();
-  }
 }
 
 interface HelpControlFactory {
   /** @return HelpControl */
   function create();
-}
-
-namespace Nexendrie\Components\Help;
-
-/**
- * @author Jakub Konečný
- */
-class HelpPagesStorage extends \Nette\Utils\ArrayList {
-  /**
-   * @param mixed $index
-   * @param HelpPage $page
-   * @throws \InvalidArgumentException
-   */
-  function offsetSet($index, $page) {
-    if(!$page instanceof HelpPage) throw new \InvalidArgumentException("Argument must be of type HelpPage.");
-    parent::offsetSet($index, $page);
-  }
-  
-  /**
-   * @param string $slug
-   * @return bool
-   */
-  function hasPage($slug) {
-    foreach($this as $page) {
-      if($page->slug === $slug) return true;
-    }
-    return false;
-  }
-  
-  /**
-   * @param string $slug
-   * @return int|NULL
-   */
-  function getIndex($slug) {
-    foreach($this as $index => $page) {
-      if($page->slug === $slug) return $index;
-    }
-    return NULL;
-  }
-}
-
-/**
- * @author Jakub Konečný
- * @property-read string $slug
- * @property-read string $title
- */
-class HelpPage extends \Nette\Object {
-  /** @var string */
-  protected $slug;
-  /** @var string */
-  protected $title;
-  
-  /**
-   * @param string $slug
-   * @param string $title
-   */
-  function __construct($slug, $title) {
-    $this->slug = $slug;
-    $this->title = $title;
-  }
-  
-  function getSlug() {
-    return $this->slug;
-  }
-
-  function getTitle() {
-    return $this->title;
-  }
 }
 ?>
