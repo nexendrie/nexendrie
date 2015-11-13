b070d9674a245d6b3c78ede995757d7442594ae5
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-11-13 15:22:55 +0100

    increased received income tax from town if the owner has castle

--- app/orm/Castle/Castle.php
+++ app/orm/Castle/Castle.php
@@ -12,6 +12,7 @@ namespace Nexendrie\Orm;
  * @property User $owner {1:1 User::$castle}
  * @property int $level {default 1}
  * @property int $hp {default 100}
+ * @property-read int $taxesBonusIncome {virtual}
  */
 class Castle extends \Nextras\Orm\Entity\Entity {
   const MAX_LEVEL = 5;
@@ -37,6 +38,11 @@ class Castle extends \Nextras\Orm\Entity\Entity {
   
   protected function getterFoundedAt() {
     return $this->localeModel->formatDate($this->founded);
+  }
+  
+  protected function getterTaxesBonusIncome() {
+    if($this->hp <= 30) return 0;
+    else return $this->level * 30;
   }
 }
 ?>
