071a8a8b7776b6fd108f03e0ff6a8c9b6e6a55e2
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-11-24 21:09:49 +0100

    order items in shop by their strength

--- app/orm/Shop/Shop.php
+++ app/orm/Shop/Shop.php
@@ -10,7 +10,7 @@ use Nextras\Orm\Relationships\OneHasMany;
  * @author Jakub Konečný
  * @property string $name
  * @property string $description
- * @property OneHasMany|Item[] $items {1:m Item}
+ * @property OneHasMany|Item[] $items {1:m Item order:strength}
  */
 class Shop extends \Nextras\Orm\Entity\Entity {
 
