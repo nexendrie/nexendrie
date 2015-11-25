d3cf38e6e690aa56286c17f7b85e6b2d06ee2b16
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-11-25 16:13:04 +0100

    showed enchantment level and bonus for equipment at page property/equipment

--- app/orm/UserItem/UserItem.php
+++ app/orm/UserItem/UserItem.php
@@ -9,11 +9,25 @@ namespace Nexendrie\Orm;
  * @property User $user {m:1 User::$items}
  * @property int $amount {default 1}
  * @property bool $worn {default 0}
+ * @property int $level {default 0}
+ * @property-read int $maxLevel {virtual}
  */
 class UserItem extends \Nextras\Orm\Entity\Entity {
   protected function setterAmount($value) {
     if($value < 0) return 0;
     else return $value;
+  }
+  
+  protected function setterLevel($value) {
+    if($value < 0) return 0;
+    elseif($value > $this->maxLevel) return $this->maxLevel;
+    else return $value;
+  }
+  
+  protected function getterMaxLevel() {
+    if(!in_array($this->item->type, Item::getEquipmentTypes())) return 0;
+    elseif($this->item->strength < 4) return 2;
+    else return 3;
   }
 }
 ?>
