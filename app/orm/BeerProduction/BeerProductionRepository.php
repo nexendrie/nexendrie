540944560109bbbb82998f5d1ece941a3b7d73b7
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-12-10 19:41:58 +0100

    showed income from beer production in budget

--- app/orm/BeerProduction/BeerProductionRepository.php
+++ app/orm/BeerProduction/BeerProductionRepository.php
@@ -14,5 +14,24 @@ class BeerProductionRepository extends \Nextras\Orm\Repository\Repository {
   static function getEntityClassNames() {
     return array("Nexendrie\Orm\BeerProduction");
   }
+  
+  /**
+   * Get beer made this month by specified user
+   * 
+   * @param int $user
+   * @return ICollection|MonasteryDonation[]
+   */
+  function findProducedThisMonth($user) {
+    $month = date("n");
+    $year = date("Y");
+    $startOfMonthTS = mktime(0, 0, 0, $month, 1, $year);
+    $date = new \DateTime;
+    $date->setTimestamp($startOfMonthTS);
+    $start = $date->getTimestamp();
+    $date->modify("+ 1 month");
+    $date->modify("- 1 second");
+    $end = $date->getTimestamp();
+    return $this->findBy(array("user" => $user, "when>" => $start, "when<" => $end));
+  }
 }
 ?>
