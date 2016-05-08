27109f47c25b68d38e35ff2cef0a5f4d3243631e
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-05-08 15:53:08 +0200

    broke Combat::userCombatStats() into separate function for each stat

--- app/model/Combat.php
+++ app/model/Combat.php
@@ -21,6 +21,65 @@ class Combat extends \Nette\Object {
   }
   
   /**
+   * Calculate specified user's life and max life
+   * 
+   * @param UserEntity $user
+   * @return int[]
+   */
+  function calculateUserLife(UserEntity $user) {
+    $maxLife = $life = $user->maxLife;
+    $helmet = $this->inventoryModel->getHelmet($user->id);
+    if($helmet) {
+      $hpIncrease = ($helmet->item->strength + $helmet->level) * 5;
+      $maxLife += $hpIncrease;
+      $life  += $hpIncrease;
+    }
+    return array("maxLife" => $maxLife, "life" => $life);
+  }
+  
+  /**
+   * Calculate specified user's damage
+   * 
+   * @param UserEntity $user
+   * @param MountEntity|NULL $mount
+   * @return int
+   */
+  function calculateUserDamage(UserEntity $user, MountEntity $mount = NULL) {
+    $damage = 0;
+    $weapon = $this->inventoryModel->getWeapon($user->id);
+    if($weapon) $damage += $weapon->item->strength + $weapon->level;
+    $damageSkills = $this->orm->userSkills->findByUserAndStat($user->id, "damage");
+    if($damageSkills) {
+      foreach($damageSkills as $damageSkill) {
+        $damage += $damageSkill->skill->statIncrease * $damageSkill->level;
+      }
+    }
+    if($mount) $damage += $mount->damage;
+    return $damage;
+  }
+  
+  /**
+   * Calculate specified user's armor
+   * 
+   * @param UserEntity $user
+   * @param MountEntity|NULL $mount
+   * @return int
+   */
+  function calculateUserArmor(UserEntity $user, MountEntity $mount = NULL) {
+    $armorValue = 0;
+    $armor = $this->inventoryModel->getArmor($user->id);
+    if($armor) $armorValue += $armor->item->strength + $armor->level;
+    $armorSkills = $this->orm->userSkills->findByUserAndStat($user->id, "armor");
+    if($armorSkills) {
+      foreach($armorSkills as $armorSkill) {
+        $armorValue += $armorSkill->skill->statIncrease * $armorSkill->level;
+      }
+    }
+    if($mount) $armorValue += $mount->armor;
+    return $armorValue;
+  }
+  
+  /**
    * Get specified user's combat stats
    * 
    * @param UserEntity $user
@@ -28,35 +87,9 @@ class Combat extends \Nette\Object {
    * @return int[]
    */
   function userCombatStats(UserEntity $user, MountEntity $mount = NULL) {
-    $stats = array();
-    $stats["maxLife"] = $stats["life"] = $user->maxLife;
-    $stats["damage"] = $stats["armor"] = 0;
-    $weapon = $this->inventoryModel->getWeapon($user->id);
-    $armor = $this->inventoryModel->getArmor($user->id);
-    $helmet = $this->inventoryModel->getHelmet($user->id);
-    if($weapon) $stats["damage"] += $weapon->item->strength + $weapon->level;
-    if($armor) $stats["armor"] += $armor->item->strength + $armor->level;
-    if($helmet) {
-      $hpIncrease = ($helmet->item->strength + $helmet->level) * 5;
-      $stats["maxLife"] += $hpIncrease;
-      $stats["life"]  += $hpIncrease;
-    }
-    $damageSkills = $this->orm->userSkills->findByUserAndStat($user->id, "damage");
-    $armorSkills = $this->orm->userSkills->findByUserAndStat($user->id, "armor");
-    if($damageSkills) {
-      foreach($damageSkills as $damageSkill) {
-        $stats["damage"] += $damageSkill->skill->statIncrease * $damageSkill->level;
-      }
-    }
-    if($armorSkills) {
-      foreach($armorSkills as $armorSkill) {
-        $stats["armor"] += $armorSkill->skill->statIncrease * $damageSkill->level;
-      }
-    }
-    if($mount) {
-      $stats["damage"] += $mount->damage;
-      $stats["armor"] += $mount->armor;
-    }
+    $stats = $this->calculateUserLife($user);
+    $stats["damage"] = $this->calculateUserDamage($user, $mount);
+    $stats["armor"] = $this->calculateUserArmor($user, $mount);
     return $stats;
   }
 }
