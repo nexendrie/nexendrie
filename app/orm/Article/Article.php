38522c32decc3c33aa582f3a7e7195a713469a08
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-05-21 17:45:51 +0200

    added more categories for articles

--- app/orm/Article/Article.php
+++ app/orm/Article/Article.php
@@ -20,6 +20,12 @@ use Nextras\Orm\Relationships\OneHasMany;
 class Article extends \Nextras\Orm\Entity\Entity {
   const CATEGORY_NEWS = "news";
   const CATEGORY_CHRONICLE = "chronicle";
+  const CATEGORY_POETRY = "poetry";
+  const CATEGORY_SHORT_STORY = "short_story";
+  const CATEGORY_ESSAY = "essay";
+  const CATEGORY_NOVELLA = "novella";
+  const CATEGORY_FAIRY_TALE = "fairy_tale";
+  const CATEGORY_UNCATEGORIZED = "uncategorized";
   
   /** @var \Nexendrie\Model\Locale */
   protected $localeModel;
@@ -35,6 +41,12 @@ class Article extends \Nextras\Orm\Entity\Entity {
     return array(
       self::CATEGORY_NEWS => "Novinky",
       self::CATEGORY_CHRONICLE => "Kronika",
+      self::CATEGORY_POETRY => "Poezie",
+      self::CATEGORY_SHORT_STORY => "Povídky",
+      self::CATEGORY_ESSAY => "Eseje",
+      self::CATEGORY_NOVELLA => "Novely",
+      self::CATEGORY_FAIRY_TALE => "Pohádky",
+      self::CATEGORY_UNCATEGORIZED => "Nezařazené",
     );
   }
   
