a6da865128f405297b25e4e80c916db678e51e34
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-10-14 17:02:54 +0200

    consider current environment in ci/prepare_deploy.php

--- ci/prepare_deploy.php
+++ ci/prepare_deploy.php
@@ -6,6 +6,16 @@ require WWW_DIR . "/vendor/autoload.php";
 
 $filename = WWW_DIR . "/deployment.ini";
 $config = file_get_contents($filename);
+if(!isset($_ENV["DEPLOY_ENVIRONMENT"])) {
+  echo "Error: deploy environment is not set.";
+  exit(1);
+}
+switch(getenv("DEPLOY_ENVIRONMENT")) {
+  case "alpha":
+    $user = getenv("FTP_ALPHA_USER");
+    $password = getenv("FTP_ALPHA_PASSWORD");
+    break;
+}
 $user = getenv("FTP_ALPHA_USER");
 $password = getenv("FTP_ALPHA_PASSWORD");
 $config .= "user=$user
