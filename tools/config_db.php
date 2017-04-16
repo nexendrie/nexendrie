1056c4b6578ba856fa2bc9729f9120b59321b799
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-04-16 14:02:14 +0200

    phing: added target config-db, this is part of #38

---
+++ tools/config_db.php
@@ -0,0 +1,22 @@
+<?php
+use Nette\Neon\Neon,
+    Nette\Utils\Arrays;
+
+const WWW_DIR = __DIR__ . "/..";
+const APP_DIR = WWW_DIR . "/app";
+
+require WWW_DIR . "/vendor/autoload.php";
+
+$filename = APP_DIR . "/config/local.neon";
+$db = [
+  "driver" => Arrays::get($argv, 1, "mysql"),
+  "host" => Arrays::get($argv, 2, "localhost"),
+  "dbname" => Arrays::get($argv, 3, "nexendrie"),
+  "user" => Arrays::get($argv, 4, "nexendrie"),
+  "password" => Arrays::get($argv, 5, "nexendrie"),
+];
+
+$config = Neon::decode(file_get_contents($filename));
+$config["dbal"] = $db;
+file_put_contents($filename, Neon::encode($config, Neon::BLOCK));
+?>
