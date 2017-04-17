c88bcf169e8c6c265a873b78adbb0346a0cc64ad
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-04-17 15:46:18 +0200

    removed a condition in tools/prepare_deploy.php

--- tools/prepare_deploy.php
+++ tools/prepare_deploy.php
@@ -6,10 +6,6 @@ require WWW_DIR . "/vendor/autoload.php";
 
 $filename = WWW_DIR . "/deployment.ini";
 $config = file_get_contents($filename);
-if(!isset($_ENV["DEPLOY_ENVIRONMENT"])) {
-  echo "Error: deploy environment is not set.";
-  exit(1);
-}
 switch(getenv("DEPLOY_ENVIRONMENT")) {
   case "alpha":
     $user = getenv("FTP_ALPHA_USER");
