bf51a4be23fba90176dd9e9b9008f8c837d56762
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-06-13 14:42:55 +0200

    fixed a variable name in JobMessagesPresenter::createComponentAddJobMessageForm()

--- app/Presenters/AdminModule/JobMessagesPresenter.php
+++ app/Presenters/AdminModule/JobMessagesPresenter.php
@@ -59,7 +59,7 @@ class JobMessagesPresenter extends BasePresenter {
   protected function createComponentAddJobMessageForm(AddEditJobMessageFormFactory $factory): Form {
     $form = $factory->create();
     $form->onSuccess[] = function(Form $form, array $values) {
-      $data["job"] = $this->job->id;
+      $values["job"] = $this->job->id;
       $this->model->addMessage($values);
       $this->flashMessage("Hláška přidána.");
       $this->redirect("list", ["id" => $this->job->id]);
