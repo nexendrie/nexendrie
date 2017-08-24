7ca0a493ef2d85ddcdb29504ad96822fec3d9d6a
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-08-24 13:01:10 +0200

    ignore errors in sql when cleaning database

--- tools/clean_db.php
+++ tools/clean_db.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
 
-use Nette\Neon\Neon;
+use Nette\Neon\Neon,
+    Nextras\Dbal\QueryException;
 
 require __DIR__ . "/../vendor/autoload.php";
 
@@ -9,9 +10,13 @@ $config = Neon::decode(file_get_contents(__DIR__ . "/../tests/local.neon"));
 
 $connection = new Nextras\Dbal\Connection($config["dbal"]);
 
-$connection->query("SET foreign_key_checks = 0");
-$tables = $connection->query("SHOW TABLES");
-foreach($tables as $table) {
-  $connection->query("DROP TABLE $table->Tables_in_nexendrie_test");
+try {
+  $connection->query("SET foreign_key_checks = 0");
+  $tables = $connection->query("SHOW TABLES");
+  foreach($tables as $table) {
+    $connection->query("DROP TABLE $table->Tables_in_nexendrie_test");
+  }
+} catch(QueryException $e) {
+
 }
 ?>
