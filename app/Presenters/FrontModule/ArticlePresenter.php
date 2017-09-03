039b227dcfb81449426f3a5cc359f544618ed703
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-09-03 12:28:56 +0200

    always use anonymous functions as callbacks for forms

--- app/Presenters/FrontModule/ArticlePresenter.php
+++ app/Presenters/FrontModule/ArticlePresenter.php
@@ -31,21 +31,19 @@ class ArticlePresenter extends BasePresenter {
   
   protected function createComponentAddCommentForm(AddCommentFormFactory $factory): Form {
     $form = $factory->create();
-    $form->onSuccess[] = [$this, "addCommentFormSucceeded"];
+    $form->onSuccess[] = function(Form $form, array $values) {
+      $values["article"] = $this->getParameter("id");
+      try {
+        $this->model->addComment($values);
+        $this->flashMessage("Komentář přidán.");
+      } catch(AuthenticationNeededException $e) {
+        $this->flashMessage("Pro přidání komentáře musíš být přihlášený.");
+        $this->redirect("User:login");
+      } catch(MissingPermissionsException $e) {
+        $this->flashMessage("Nemůžeš přidávat komentáře.");
+      }
+    };
     return $form;
-  }
-  
-  public function addCommentFormSucceeded(Form $form, array $values): void {
-    $values["article"] = $this->getParameter("id");
-    try {
-      $this->model->addComment($values);
-      $this->flashMessage("Komentář přidán.");
-    } catch(AuthenticationNeededException $e) {
-      $this->flashMessage("Pro přidání komentáře musíš být přihlášený.");
-      $this->redirect("User:login");
-    } catch(MissingPermissionsException $e) {
-      $this->flashMessage("Nemůžeš přidávat komentáře.");
-    }
   }
 }
 ?>
