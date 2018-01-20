2ff90e02577e5dc1d0398c616359dc24f3802161
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-01-20 15:34:41 +0100

    separated different action in MountsStatusTask::run() into new methods

--- app/Cron/MountsStatusTask.php
+++ app/Cron/MountsStatusTask.php
@@ -20,23 +20,31 @@ class MountsStatusTask {
     $this->orm = $orm;
   }
   
+  protected function decreaseHitpoints(MountEntity $mount): void {
+    $mount->hp -= 5;
+    echo "Decreasing (#$mount->id) $mount->name's life by 5.";
+  }
+  
+  protected function makeAdult(MountEntity $mount): void {
+    $twoMonths = 60 * 60 * 24 * 30 * 2;
+    if($mount->gender === MountEntity::GENDER_YOUNG AND $mount->birth + $twoMonths < time()) {
+      echo "The mount is too old. It becomes adult.";
+      $roll = mt_rand(0, 1);
+      $mount->gender = ($roll === 0) ? MountEntity::GENDER_MALE : MountEntity::GENDER_FEMALE;
+    }
+  }
+  
   /**
    * @cronner-task Mounts status update
    * @cronner-period 1 week
    * @cronner-time 01:00 - 02:00
    */
   public function run(): void {
-    $twoMonths = 60 * 60 * 24 * 30 * 2;
     echo "Starting mounts status update ...\n";
     $mounts = $this->orm->mounts->findOwnedMounts();
     foreach($mounts as $mount) {
-      $mount->hp -= 5;
-      echo "Decreasing (#$mount->id) $mount->name's life by 5.";
-      if($mount->gender === MountEntity::GENDER_YOUNG AND $mount->birth + $twoMonths < time()) {
-        echo "The mount is too old. It becomes adult.";
-        $roll = mt_rand(0, 1);
-        $mount->gender = ($roll === 0) ? MountEntity::GENDER_MALE : MountEntity::GENDER_FEMALE;
-      }
+      $this->decreaseHitpoints($mount);
+      $this->makeAdult($mount);
       $this->orm->mounts->persist($mount);
       echo "\n";
     }
