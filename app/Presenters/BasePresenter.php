410d4808a835e268095ba394a3ad8b66f05bdb09
Jakub Konečný <jakub.konecny2@centrum.cz>
2019-09-14 19:18:09 +0200

    add Content-Language header for all pages

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -73,6 +73,7 @@ abstract class BasePresenter extends \Nette\Application\UI\Presenter {
   }
   
   protected function beforeRender(): void {
+    $this->getHttpResponse()->setHeader("Content-Language", "cs,sk");
     $this->template->versionSuffix = $this->sr->settings["site"]["versionSuffix"];
   }
   
