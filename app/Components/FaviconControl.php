253e658d1ab8c2e391bc2b15789d60ece5701f90
Jakub Konečný <jakub.konecny2@centrum.cz>
2019-09-28 17:20:30 +0200

    added some documentation comments

--- app/Components/FaviconControl.php
+++ app/Components/FaviconControl.php
@@ -3,6 +3,12 @@ declare(strict_types=1);
 
 namespace Nexendrie\Components;
 
+/**
+ * FaviconControl
+ *
+ * @author Jakub Konečný
+ * @property-read \Nette\Bridges\ApplicationLatte\Template $template
+ */
 final class FaviconControl extends \Nette\Application\UI\Control {
   public function render(): void {
     $this->template->setFile(__DIR__ . "/favicon.latte");
