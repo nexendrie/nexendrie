6a6b1890f701290e5cacaf44088d4f0c47207099
Jakub Konečný <jakub.konecny2@centrum.cz>
2019-10-13 11:49:27 +0200

    renamed a variable in homepage template

--- app/Presenters/FrontModule/HomepagePresenter.php
+++ app/Presenters/FrontModule/HomepagePresenter.php
@@ -20,7 +20,7 @@ final class HomepagePresenter extends BasePresenter {
   public function renderPage(int $page = 1): void {
     $paginator = new \Nette\Utils\Paginator();
     $paginator->page = $page;
-    $this->template->news = $this->model->listOfNews($paginator);
+    $this->template->articles = $this->model->listOfNews($paginator);
     $this->template->paginator = $paginator;
   }
 }
