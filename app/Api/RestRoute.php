6d7c638aaee56b41fe1b422454cb0d0ff44e25ca
Jakub Konečný <jakub.konecny2@centrum.cz>
2019-10-13 10:49:53 +0200

    tweaked RestRout::detectAction()

--- app/Api/RestRoute.php
+++ app/Api/RestRoute.php
@@ -6,15 +6,11 @@ namespace Nexendrie\Api;
 use Nette\Http\IRequest;
 
 final class RestRoute extends \AdamStipak\RestRoute {
-  protected function detectAction(IRequest $request) {
-    try {
-      return parent::detectAction($request);
-    } catch(\Nette\InvalidStateException $e) {
-      if($this->detectMethod($request) === "HEAD") {
-        return "read";
-      }
-      throw $e;
+  protected function detectAction(IRequest $request): string {
+    if($this->detectMethod($request) === "HEAD") {
+      return "read";
     }
+    return parent::detectAction($request);
   }
 }
 ?>
