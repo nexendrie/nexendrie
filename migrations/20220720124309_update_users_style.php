8b3c69eb69d38227f975726bf829a8bafcbe3b42
Jakub Konečný <jakub.konecny2@centrum.cz>
2022-07-20 14:57:12 +0200

    added database migration for updating users' styles dark sky and blue sky are being replaced by nexendrie

---
+++ migrations/20220720124309_update_users_style.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class UpdateUsersStyle extends AbstractMigration {
+  public function change(): void {
+    if(!$this->isMigratingUp()) {
+      return;
+    }
+    $this->execute("UPDATE users SET style='nexendrie' WHERE style IN ('dark-sky', 'blue-sky')");
+  }
+}
+?>
