3be6c5261435321b249b8b9feb9cfb8f1ef49b2d
Jakub Konečný <jakub.konecny2@centrum.cz>
2025-12-16 17:49:55 +0100

    removed deprecated styles

--- tests/Nexendrie/Model/ThemesManagerTest.php
+++ tests/Nexendrie/Model/ThemesManagerTest.php
@@ -23,13 +23,9 @@ final class ThemesManagerTest extends \Tester\TestCase
     {
         $result = $this->model->getList();
         Assert::type("array", $result);
-        Assert::count(4, $result);
-        Assert::contains("dark-sky", array_keys($result));
-        Assert::same("Temná obloha (zavržený)", $result["dark-sky"]);
+        Assert::count(2, $result);
         Assert::contains("nexendrie", array_keys($result));
         Assert::same("Nexendrie", $result["nexendrie"]);
-        Assert::contains("blue-sky", array_keys($result));
-        Assert::same("Modrá obloha (zavržený)", $result["blue-sky"]);
         Assert::contains("matrix", array_keys($result));
         Assert::same("Matrix (experimentální)", $result["matrix"]);
     }
