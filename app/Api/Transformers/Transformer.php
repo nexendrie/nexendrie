6a1ec7225c4553e139ec6ffb39de7f30f94d1fbf
Jakub Konečný <jakub.konecny2@centrum.cz>
2026-02-28 14:08:54 +0100

    removed new documentation comments

--- app/Api/Transformers/Transformer.php
+++ app/Api/Transformers/Transformer.php
@@ -9,13 +9,7 @@ interface Transformer
 {
     public function transform(Entity $entity, int $maxDepth, string $apiVersion): \stdClass;
 
-    /**
-     * @return class-string
-     */
     public function getEntityClassName(): string;
 
-    /**
-     * @return class-string
-     */
     public function getCollectionName(): string;
 }
